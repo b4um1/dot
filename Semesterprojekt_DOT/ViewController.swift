2d67c17fc21b9670d3157459b79f16f57753a70f
Mario Baumgartner <mario.baumgartner92@gmx.at>
2015-05-30 14:24:24 +0200

    another merge with multipeer

--- Semesterprojekt_DOT/ViewController.swift
+++ Semesterprojekt_DOT/ViewController.swift
@@ -25,16 +25,9 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate {
         appDelegate.mpcHandler.advertiseSelf(true)
         
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "peerChangedStateWithNotification:", name: "MPC_DidChangeStateNotification", object: nil)
-        
-        //NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleReceivedDataWithNotification:", name: "MPC_DidReceiveDataNotification", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handleReceivedDataWithNotification:", name: "MPC_DidReceiveDataNotification", object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "bluetoothStateDidChange:", name: "MPC_BluetoothStateChanged", object: nil)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    
     
     func peerChangedStateWithNotification(notification:NSNotification){
         let userInfo = NSDictionary(dictionary: notification.userInfo!)
@@ -42,11 +35,63 @@ class ViewController: UIViewController, MCBrowserViewControllerDelegate {
         let state = userInfo.objectForKey("state") as! Int
         let peerid = userInfo.objectForKey("peerID") as! MCPeerID
         
-        println("state: \(state) peerID: \(peerid)")
-        if state != MCSessionState.Connecting.rawValue{ //toRaw was replaced with rawValue
+        if state == MCSessionState.Connecting.rawValue{
+            self.label_pairedpartner.text = "Connecting with \(peerid.displayName)..."
+        }
+        if state == MCSessionState.Connected.rawValue{ //toRaw was replaced with rawValue
             self.label_pairedpartner.text = "Connected with \(peerid.displayName)"
+        }else{
+            self.label_pairedpartner.text = "Disconnected"
         }
         
+    }
+    
+    func bluetoothStateDidChange(notification: NSNotification){ //1 on 0 off
+        let info = NSDictionary(dictionary: notification.userInfo!)
+        let state = info.objectForKey("state") as! Int
+        
+        switch(state){
+        case 0:
+            appDelegate.mpcHandler.session.disconnect()
+            break
+        default:
+            break
+        }
+    }
+    
+    func handleReceivedDataWithNotification(notification:NSNotification){
+        let userInfo = notification.userInfo! as Dictionary
+        let receivedData:NSData = userInfo["data"] as! NSData
+        
+        let message = NSJSONSerialization.JSONObjectWithData(receivedData, options: NSJSONReadingOptions.AllowFragments, error: nil) as! NSDictionary
+        let senderPeerId:MCPeerID = userInfo["peerID"] as! MCPeerID
+        let senderDisplayName = senderPeerId.displayName
+        
+        if message.objectForKey("string")?.isEqualToString("New Game") == true{
+            let alert = UIAlertController(title: "TicTacToe", message: "\(senderDisplayName) has started a new Game", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            
+            self.presentViewController(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    @IBAction func startNewGame(sender: AnyObject) {
+        println("Message has been sent");
+        
+        let messageDict = ["string":"New Game"]
+        
+        let messageData = NSJSONSerialization.dataWithJSONObject(messageDict, options: NSJSONWritingOptions.PrettyPrinted, error: nil)
+        
+        var error:NSError?
+        
+        appDelegate.mpcHandler.session.sendData(messageData, toPeers: appDelegate.mpcHandler.session.connectedPeers, withMode: MCSessionSendDataMode.Reliable, error: &error)
+        
+        if error != nil{
+            println("error: \(error?.localizedDescription)")
+        }
     }
     
     @IBAction func connectWithOpponentPlayer(sender: AnyObject) {
