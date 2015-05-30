680ccd87ef4737c76ce0c664d6f8d1ca5f4076bd
Mario Baumgartner <mario.baumgartner92@gmx.at>
2015-05-30 10:52:19 +0200

    update of appdelegate

--- Semesterprojekt_DOT/ViewController.swift
+++ Semesterprojekt_DOT/ViewController.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import MultipeerConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MCBrowserViewControllerDelegate {
 
     var test = 0; //Testvariable
+    var appDelegate: AppDelegate!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.mpcHandler.setupPeerWithDisplayName(UIDevice.currentDevice().name)
+        appDelegate.mpcHandler.setupSession()
+        appDelegate.mpcHandler.advertiseSelf(true)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func connectWithOpponentPlayer(sender: AnyObject) {
+        if appDelegate.mpcHandler.session != nil{
+            appDelegate.mpcHandler.setupBrowser()
+            appDelegate.mpcHandler.browser.delegate = self
+            
+            self.presentViewController(appDelegate.mpcHandler.browser, animated: true, completion: nil)
+            
+        }
+
+    }
+    
+    func browserViewControllerDidFinish(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func browserViewControllerWasCancelled(browserViewController: MCBrowserViewController!) {
+        appDelegate.mpcHandler.browser.dismissViewControllerAnimated(true, completion: nil)
     }
 
 
