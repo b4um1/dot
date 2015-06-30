8cdd45f1f7d85517a2715e74c41a583b87528cbd
Mario Baumgartner <mario.baumgartner92@gmx.at>
2015-06-30 12:19:54 +0200

    added new image of action dot

--- Semesterprojekt_DOT/GameButton.swift
+++ Semesterprojekt_DOT/GameButton.swift
@@ -14,7 +14,7 @@ class GameButton: UIButton {
     var image_standard = UIImage(named:"dot_standard")
     var image_move = UIImage(named:"dot_move")
     var image_locked = UIImage(named:"dot_locked")
-    var image_action = UIImage(named:"dot_standard")
+    var image_action = UIImage(named:"dot_action")
     
     var isLocked = false
     var isMove = false
@@ -27,24 +27,24 @@ class GameButton: UIButton {
     
     func setImageMove(){
         self.setImage(image_move, forState: .Normal)
-        self.alpha = 1.0
+        //self.alpha = 1.0
         isMove = true
         isAction = false
     }
     func setImageStandard(){
         self.setImage(image_standard, forState: .Normal)
-        self.alpha = 1.0
+        //self.alpha = 1.0
         isLocked = false
         isAction = false
     }
     func setImageLocked(){
         self.setImage(image_locked, forState: .Normal)
-        self.alpha = 1.0
+        //self.alpha = 1.0
         isLocked = true
     }
     func setImageAction() {
         self.setImage(image_action, forState: .Normal)
-        self.alpha = 0.5
+        //self.alpha = 0.5
         isAction = true
     }
 }
